a9a07c100097d880178458ebf3bb202e2009a8de
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-05-08 00:01:06 +1000

    Redirect to blog.lunaa.me

--- js/addAnchors.900a55f34b82a4887d44691f64eb98fabe178109dee00728f5fff59494ca3a60.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-  const headings = document.querySelectorAll("h2, h3, h4, h5, h6");
-  for (const heading of headings) {
-    heading.innerHTML =
-      '<a href="#' + heading.id + '">' + heading.innerHTML + "</a>";
-  }
-};
